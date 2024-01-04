983a5a656efda06bc489fd86ae7638e2db33c2de
audreyllamas <155239843+audreyllamas@users.noreply.github.com>
2024-01-04 23:37:27 +0800

    Lots of changes Done

--- app/src/main/java/com/example/ui_pupmunchmapp/LoginPage.java
+++ app/src/main/java/com/example/ui_pupmunchmapp/LoginPage.java
@@ -2,7 +2,10 @@ package com.example.ui_pupmunchmapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginPage extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class LoginPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+        TextView btn=findViewById(R.id.txtViewSignUp);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginPage.this, SignupActivity.class));
+            }
+        });
+
+
+
     }
 }

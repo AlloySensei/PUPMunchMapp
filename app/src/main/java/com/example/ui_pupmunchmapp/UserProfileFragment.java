f5d83e13a15f587ec1feab45c342c1338b627872
hzlcncpcn <hazelconcepcion205@gmail.com>
2024-01-05 23:54:24 +0800

    Added main navigation menu

--- app/src/main/java/com/example/ui_pupmunchmapp/UserProfileFragment.java
+++ app/src/main/java/com/example/ui_pupmunchmapp/UserProfileFragment.java
@@ -15,12 +15,10 @@ import android.view.ViewGroup;
  */
 public class UserProfileFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -36,7 +34,7 @@ public class UserProfileFragment extends Fragment {
      * @param param2 Parameter 2.
      * @return A new instance of fragment UserProfileFragment.
      */
-    // TODO: Rename and change types and number of parameters
+
     public static UserProfileFragment newInstance(String param1, String param2) {
         UserProfileFragment fragment = new UserProfileFragment();
         Bundle args = new Bundle();

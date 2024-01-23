37ee77b254e3a2dcc9bfb388b0c9e683651d7539
Alloysius <alloysius86@gmail.com>
2024-01-23 23:23:15 +0800

    Updated to catch error messages in sign up

--- app/src/main/java/com/example/ui_pupmunchmapp/LoginPage.java
+++ app/src/main/java/com/example/ui_pupmunchmapp/LoginPage.java
@@ -5,16 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class LoginPage extends AppCompatActivity {
+    Button buttonLogin;
+    TextView signUp;
+    EditText username,password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
-        TextView btn=findViewById(R.id.txtViewSignUp);
-        btn.setOnClickListener(new View.OnClickListener() {
+        signUp = findViewById(R.id.txtViewSignUp);
+        username = findViewById(R.id.edPassword);
+        password = findViewById(R.id.edUsername);
+        buttonLogin = findViewById(R.id.buttonLogin);
+
+
+        signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(LoginPage.this, SignupActivity.class));
--- app/src/main/java/com/example/ui_pupmunchmapp/SignupActivity.java
+++ app/src/main/java/com/example/ui_pupmunchmapp/SignupActivity.java
@@ -3,13 +3,70 @@ package com.example.ui_pupmunchmapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class SignupActivity extends AppCompatActivity {
-
+    Button buttonReg;
+    EditText editTextName, editTextEmail, editTextPassword, editTextPasswordConfirm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        editTextName = findViewById(R.id.inputUsername);
+        editTextEmail = findViewById(R.id.inputEmail);
+        editTextPassword = findViewById(R.id.inputPassword);
+        editTextPasswordConfirm = findViewById(R.id.inputConfirmPassword);
+        buttonReg = findViewById(R.id.button2);
+
+        editTextPasswordConfirm.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                editTextPasswordConfirm.setTextColor(getResources().getColor(android.R.color.black));
+            }
+        });
+        buttonReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name, email, password, passwordConfirm;
+
+                name = String.valueOf(editTextName.getText());
+                email = String.valueOf(editTextEmail.getText());
+                password = String.valueOf(editTextPassword.getText());
+                passwordConfirm = String.valueOf(editTextPasswordConfirm.getText());
+
+                if(name.isEmpty()){
+                    editTextName.setError("This field cannot be empty");
+                }else if(email.isEmpty()){
+                    editTextEmail.setError("This field cannot be empty");
+                }
+                if (password.equals(passwordConfirm)) {
+                    editTextPasswordConfirm.setError(null);
+                } else {
+                    editTextPasswordConfirm.setError("Passwords do not match");
+                    editTextPasswordConfirm.setTextColor(getResources().getColor(android.R.color.holo_red_light));
+                }
+
+
+            }
+        });
+
 
     }
 }

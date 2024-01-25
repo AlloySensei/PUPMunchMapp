0d62516641f4e9b91f557b80541696ca83e5188f
Alloysius <alloysius86@gmail.com>
2024-01-25 13:21:13 +0800

    Food class for firebase

---
+++ app/src/main/java/com/example/ui_pupmunchmapp/Foods.java
@@ -0,0 +1,130 @@
+package com.example.ui_pupmunchmapp;
+
+public class Foods {
+    private int categoryId;
+    private String Description;
+    private boolean BestFood;
+    private int id;
+    private int locationId;
+    private double price;
+    private String imagePath;
+    private String priceId;
+    private double star;
+    private int timeId;
+    private int timeValue;
+    private String title;
+    private int numberInCart;
+
+    public String getPriceId() {
+        return priceId;
+    }
+
+    public void setPriceId(String priceId) {
+        this.priceId = priceId;
+    }
+
+    public double getStar() {
+        return star;
+    }
+
+    public void setStar(double star) {
+        this.star = star;
+    }
+
+    public Foods(){
+
+    }
+
+    @Override
+    public String toString() {
+        return title;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public boolean isBestFood() {
+        return BestFood;
+    }
+
+    public void setBestFood(boolean bestFood) {
+        BestFood = bestFood;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getLocationId() {
+        return locationId;
+    }
+
+    public void setLocationId(int locationId) {
+        this.locationId = locationId;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public int getTimeId() {
+        return timeId;
+    }
+
+    public void setTimeId(int timeId) {
+        this.timeId = timeId;
+    }
+
+    public int getTimeValue() {
+        return timeValue;
+    }
+
+    public void setTimeValue(int timeValue) {
+        this.timeValue = timeValue;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getNumberInCart() {
+        return numberInCart;
+    }
+
+    public void setNumberInCart(int numberInCart) {
+        this.numberInCart = numberInCart;
+    }
+}

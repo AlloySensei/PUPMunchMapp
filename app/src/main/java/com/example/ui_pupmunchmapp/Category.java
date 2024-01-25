5eb3b9b05bea9ce9d3e395466ea26d13e09d1047
Alloysius <alloysius86@gmail.com>
2024-01-25 13:20:08 +0800

    Created category class for firebase.

---
+++ app/src/main/java/com/example/ui_pupmunchmapp/Category.java
@@ -0,0 +1,31 @@
+package com.example.ui_pupmunchmapp;
+
+public class Category {
+    private int id;
+    private String imagePath;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
